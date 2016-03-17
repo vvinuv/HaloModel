c4a61c0ab1c7f57360d69be25e624330a7f13c8b
vvinuv <vvikraman@datastar.hep.anl.gov>
2016-03-17 13:59:09 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# Authors: Vinu Vikraman <vvinuv@gmail.com>
+from distutils.core import setup
+
+setup(
+    name="halosz",
+    version="0.1-dev",
+    description="Estimating the correlation function of lensing vs SZ using halo model",
+    maintainer="Vinu Vikraman",
+    maintainer_email="vvinuv@gmail.com",
+    license="",
+    url='https://github.com/vvinuv/HaloModel',
+    packages=['halosz',],
+    classifiers=[
+        'Intended Audience :: Dark Energy Survey',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Development Status :: 1 - Alpha',
+    ],
+)
